5d2936f681fbcf86dc7c82cb0eeb88a6386533f0
HERGOINC <sales@hergo.jp>
2023-05-07 17:28:39 +0900

    Initial Commit

---
+++ BabyTime/BabyTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyTimeApp.swift
+//  BabyTime
+//
+//  Created by Daisuke Yanagi on 2023/05/07.
+//
+
+import SwiftUI
+
+@main
+struct BabyTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
