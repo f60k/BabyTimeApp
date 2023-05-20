4cba5acefa27d8b1145ff0fc0633314413dc81dc
HERGOINC <sales@hergo.jp>
2023-05-21 02:41:18 +0900

    ヘルスケアから複数件の睡眠データを同期できる

--- BabyTime/StopWatchManager.swift
+++ BabyTime/StopWatchManager.swift
@@ -39,6 +39,84 @@ struct DurationData:Identifiable
     var category:SleepCategory = .hirune
 }
 
+
+
+class Time: Comparable, Equatable {
+init(_ date: Date) {
+    //get the current calender
+    let calendar = Calendar.current
+
+    //get just the minute and the hour of the day passed to it
+    let dateComponents = calendar.dateComponents([.hour, .minute], from: date)
+
+        //calculate the seconds since the beggining of the day for comparisions
+        let dateSeconds = dateComponents.hour! * 3600 + dateComponents.minute! * 60
+
+        //set the varibles
+        secondsSinceBeginningOfDay = dateSeconds
+        hour = dateComponents.hour!
+        minute = dateComponents.minute!
+    }
+
+    init(_ hour: Int, _ minute: Int) {
+        //calculate the seconds since the beggining of the day for comparisions
+        let dateSeconds = hour * 3600 + minute * 60
+
+        //set the varibles
+        secondsSinceBeginningOfDay = dateSeconds
+        self.hour = hour
+        self.minute = minute
+    }
+
+    var hour : Int
+    var minute: Int
+
+    var date: Date {
+        //get the current calender
+        let calendar = Calendar.current
+
+        //create a new date components.
+        var dateComponents = DateComponents()
+
+        dateComponents.hour = hour
+        dateComponents.minute = minute
+
+        return calendar.date(byAdding: dateComponents, to: Date())!
+    }
+
+    /// the number or seconds since the beggining of the day, this is used for comparisions
+    private let secondsSinceBeginningOfDay: Int
+
+    //comparisions so you can compare times
+    static func == (lhs: Time, rhs: Time) -> Bool {
+        return lhs.secondsSinceBeginningOfDay == rhs.secondsSinceBeginningOfDay
+    }
+
+    static func < (lhs: Time, rhs: Time) -> Bool {
+        return lhs.secondsSinceBeginningOfDay < rhs.secondsSinceBeginningOfDay
+    }
+
+    static func <= (lhs: Time, rhs: Time) -> Bool {
+        return lhs.secondsSinceBeginningOfDay <= rhs.secondsSinceBeginningOfDay
+    }
+
+
+    static func >= (lhs: Time, rhs: Time) -> Bool {
+        return lhs.secondsSinceBeginningOfDay >= rhs.secondsSinceBeginningOfDay
+    }
+
+
+    static func > (lhs: Time, rhs: Time) -> Bool {
+        return lhs.secondsSinceBeginningOfDay > rhs.secondsSinceBeginningOfDay
+    }
+}
+
+extension Date {
+    var time: Time {
+        return Time(self)
+    }
+}
+
 class StopWatchManeger:ObservableObject{
     
     enum stopWatchMode{
@@ -145,8 +223,37 @@ class StopWatchManeger:ObservableObject{
     {
         let calender = Calendar(identifier: .gregorian)
         let elapsedTime = calender.dateComponents([.second], from: start, to: end).second!
-        let dLog = DurationData(data: Double(elapsedTime), start: start, end:end)
-        durationLog.append(dLog)
+//        self.durationLog.forEach{ item in
+////            if Calendar.current.isDate(item.start, equalTo: start, toGranularity: .nanosecond)
+////            {
+////                if Calendar.current.isDate(item.end, equalTo: end, toGranularity: .nanosecond)
+////                {
+////                    print("SKIP")
+////                    return
+////                }
+////            }
+//            if item.start.time == start.time && item.end.time == end.time
+//            {
+//
+//            }
+//        }
+        var skip = false
+        
+        for item in self.durationLog
+        {
+            if item.start.time == start.time && item.end.time == end.time
+            {
+                skip = true
+                break
+            }
+        }
+        
+        if !skip
+        {
+            let dLog = DurationData(data: Double(elapsedTime), start: start, end:end)
+            durationLog.append(dLog)
+        }
+        
     }
     
     func stop(){
